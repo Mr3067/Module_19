0f93a6c93addec8c75d6bea5b4e1cddcae5c45c1
Danilov Alex <danilov.alexandr@hotmail.com>
2024-09-13 13:58:37 +0300

    2024/03/13 00:00|Домашнее задание по теме "Модели баз данных в Django."

---
+++ Urban_Module_19/task1/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=5, decimal_places=2)
+    age = models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    size = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name='game')
+    DecimalField = models.DecimalField(max_digits=5, decimal_places=2)
+    BooleanField = models.BooleanField()
